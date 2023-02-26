4b3af30af337ab5eff27a70c06d2b8a8ed0a64ca
yousuf4you <yousuf360.ya@gmail.com>
2023-02-26 21:25:02 +0600

    complited

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export class Log {
+   static success(message: string) {
+      console.log(`%c ${message}`, "color: green");
+   }
+
+   static danger(message: string) {
+      console.log(`%c ${message}`, "color: orange");
+   }
+
+   static info(message: string) {
+      console.log(`%c ${message}`, "color: black; background: yellow");
+   }
+
+   static error(message: string) {
+      console.log(`%c ${message}`, "color: red");
+   }
+}
